ecc40befe70aa68eba8715998760a30d37d12698
genestewart <genestewart.utah@gmail.com>
2024-06-16 13:40:58 -0600

    gs

--- styles.js
+++ styles.js
@@ -1,11 +1,21 @@
 const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
+const openElements = document.querySelectorAll(".open-button");
 const closeModal = document.querySelector(".close-button");
-
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
-
+console.log('closeModal', closeModal)
 closeModal.addEventListener("click", () => {
+  console.log('Bacon')
   modal.close();
 });
+
+
+const addOpenEventListener = (element) => {
+  element.addEventListener("click", () => {
+    console.log('Eggs')
+    modal.showModal();
+  });
+}
+
+for(let i = 0; i < openElements.length; i++) {
+  addOpenEventListener(openElements[i]);
+}
+console.log('Ham')
