875e6f8dd4aeeb7789e45ff7b30d754549f5b819
genestewart <genestewart.uath@gmail.com>
2024-06-30 20:00:59 -0600

    finish

--- styles.js
+++ styles.js
@@ -3,14 +3,12 @@ const openElements = document.querySelectorAll(".open-button");
 const closeModal = document.querySelector(".close-button");
 console.log('closeModal', closeModal)
 closeModal.addEventListener("click", () => {
-  console.log('Bacon')
   modal.close();
 });
 
 
 const addOpenEventListener = (element) => {
   element.addEventListener("click", () => {
-    console.log('Eggs')
     modal.showModal();
   });
 }
