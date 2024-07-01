12b9b4ffc2b293e84535349139bbdbe1ee68a6e0
genestewart <genestewart.uath@gmail.com>
2024-06-30 18:28:40 -0600

    adding responsive sizes for the smaller devices. GS

--- styles.js
+++ styles.js
@@ -19,14 +19,17 @@ for(let i = 0; i < openElements.length; i++) {
   addOpenEventListener(openElements[i]);
 }
 
-const email1 = document.getElementById('#email');
-const email2 = document.getElementById('#email2');
-const error = document.getElementById('#error-message');
-
-email2.addEventListener(function() {
+const email1 = document.getElementById('email');
+const email2 = document.getElementById('email2');
+const error = document.getElementById('error-message');
+const verifyEmail = () => {
   if (email1.value !== email2.value) {
     error.textContent = 'Emails do not match';
   } else {
     error.textContent = '';
   }
+}
+
+email2.addEventListener('blur', ()=> {
+  verifyEmail();
 });
